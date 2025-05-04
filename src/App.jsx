90cb00608b6b6d05b03d23e79c232a407a916a5c
Andrew Tolmachev <parsoph85@gmail.com>
2025-05-04 12:09:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Rick } from "./components/Rick";
 import { EpisodeList } from "./components/presentational/EpisodeList";
+import { LocationList } from './components/LocationList';
 
 function App() {
   return (
     <>
       <EpisodeList />
+      <LocationList />
     </>
   );
 }
